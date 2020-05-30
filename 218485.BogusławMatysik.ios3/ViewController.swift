0b0a35b98f777ace6e0950f5429346b344a05cb6
Bogusław Matysik <218485@student.pwr.edu.pl>
2020-05-31 00:00:35 +0200

    Initial Commit

---
+++ 218485.BogusławMatysik.ios3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  218485.BogusławMatysik.ios3
+//
+//  Created by Bogusław Matysik on 31/05/2020.
+//  Copyright © 2020 Bogusław Matysik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 218485.BogusławMatysik.ios3Tests/_18485_Bogus_awMatysik_ios3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _18485_Bogus_awMatysik_ios3Tests.swift
+//  218485.BogusławMatysik.ios3Tests
+//
+//  Created by Bogusław Matysik on 31/05/2020.
+//  Copyright © 2020 Bogusław Matysik. All rights reserved.
+//
+
+import XCTest
+@testable import _18485_Bogus_awMatysik_ios3
+
+class _18485_Bogus_awMatysik_ios3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
